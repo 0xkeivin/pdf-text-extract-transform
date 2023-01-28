7f64324b54e659db20867367e60620d97deb2f91
0xkeivin <keivincatwork@gmail.com>
2023-01-29 00:36:46 +0800

    pdf to text working la haiyo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,10 +5,26 @@ import styles from "@/styles/Home.module.css";
 import { FileUpload } from "@/components/FileUpload";
 import axios from "axios";
 import { useEffect } from "react";
-
+import {Textarea} from "@chakra-ui/react"
+import {useState} from "react"
 const inter = Inter({ subsets: ["latin"] });
 
+
 export default function Home() {
+  const [pdfText, setPdfText] = useState("")
+
+const uploadHandler = (data: FileList | null) => {
+  console.log(data);
+  axios.get("/api/upload").then((res) => {
+    if (res.status === 200) {
+      console.log("success");
+      setPdfText(res.data.data)
+    } else {
+      console.log("error");
+      setPdfText("something went wrong :(")
+    }
+  });
+};
   useEffect(() => {
     axios.get("/api/hello").then((res) => {
       console.log(res.data);
@@ -20,9 +36,14 @@ export default function Home() {
       <Head> PDF to Image to Text ? </Head>
       <FileUpload
         accept="application/pdf"
-        onFileUploaded={(data) => {
-          console.log(data);
-        }}
+        // onFileUploaded={(data) => {
+        //   console.log(data);
+        // }}
+        onFileUploaded={uploadHandler}
+      />
+      <Textarea 
+      placeholder="Placeholder"
+      value={pdfText}
       />
     </>
   );
