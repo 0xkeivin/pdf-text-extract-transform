6b8b84492f9658de18bf8da90fd53b1903ce172a
0xkeivin <keivincatwork@gmail.com>
2023-01-28 23:39:10 +0800

    added axios to test api call

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,29 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '@/styles/Home.module.css'
-import { FileUpload } from '@/components/FileUpload'
-const inter = Inter({ subsets: ['latin'] })
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "@next/font/google";
+import styles from "@/styles/Home.module.css";
+import { FileUpload } from "@/components/FileUpload";
+import axios from "axios";
+import { useEffect } from "react";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  useEffect(() => {
+    axios.get("/api/hello").then((res) => {
+      console.log(res.data);
+    });
+  }, []);
+
   return (
     <>
       <Head> PDF to Image to Text ? </Head>
-    <FileUpload
-      accept="application/pdf"
-      onFileUploaded={(data) => {
-        console.log(data)
-      }}
-    />
+      <FileUpload
+        accept="application/pdf"
+        onFileUploaded={(data) => {
+          console.log(data);
+        }}
+      />
     </>
-  )
+  );
 }
